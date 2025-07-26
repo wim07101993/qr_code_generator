4ab694d4ce469bc65cdeba26fca8b34267d0d24f
Wim Van Laer <van.laer.wim@gmail.com>
2025-07-26 22:15:52 +0200

    chore: upgrade ios sdk

--- lib/shared/l10n/app_localizations.dart
+++ lib/shared/l10n/app_localizations.dart
@@ -95,7 +95,7 @@ abstract class AppLocalizations {
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('en'),
-    Locale('nl')
+    Locale('nl'),
   ];
 
   /// Helper text for the IBAN input field on the settings sheet of the epc qr-code
@@ -366,8 +366,9 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-      'an issue with the localizations generation tool. Please file an issue '
-      'on GitHub with a reproducible sample app and the gen-l10n configuration '
-      'that was used.');
+    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
 }
